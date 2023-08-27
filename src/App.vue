f09a6cb2e200b4c8c6c6a2fdc8778eccd907cc78
Arturo Zepeda <aaze92@gmail.com>
2023-08-26 19:53:27 -0600

    añado consulta a apli para obtener las mejores 10 criptos

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
+const criptomonedas = ref([]);
 const monedas = ref([
   { codigo: 'USD', nombre: 'Dolar Estadounidense' },
   { codigo: 'MXN', nombre: 'Peso Mexicano' },
   { codigo: 'GBP', nombre: 'Libras' },
   { codigo: 'EUR', nombre: 'Euros'}
 ]);
+
+
+onMounted(async () => {
+  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+  const respuesta = await fetch(url);
+  const resultado = await respuesta.json();
+  criptomonedas.value = resultado.Data;
+});
+
+
+
 </script>
 
 <template>
@@ -18,14 +30,24 @@ const monedas = ref([
           <label for="moneda">Moneda:</label>
           <select id="moneda">
             <option value="" disabled selected>Elige tu moneda</option>
-            <option v-for="moneda in monedas" :key="moneda.codigo" :value="moneda.codigo">{{ moneda.nombre }}</option>
+            <option 
+              v-for="moneda in monedas" 
+              :key="moneda.codigo" 
+              :value="moneda.codigo">
+              {{ moneda.nombre }}
+            </option>
           </select>
         </div>
         <div class="campo">
           <label for="criptomoneda">CriptoMoneda:</label>
           <select id="criptomoneda">
             <option value="" disabled selected>Elige tu criptoMoneda</option>
-            
+            <option 
+              v-for="criptomoneda in criptomonedas"
+              :key="criptomoneda.CoinInfo.Id"
+              :value="criptomoneda.CoinInfo.Name">
+                {{ criptomoneda.CoinInfo.FullName }}
+            </option>
           </select>
         </div>
       </form>
