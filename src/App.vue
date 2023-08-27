3be265c3500240a3ed9d83abc614199d1cd14474
Arturo Zepeda <aaze92@gmail.com>
2023-08-26 19:35:14 -0600

    añado form y empiezo con valores de moneda

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const monedas = ref([
+  { codigo: 'USD', nombre: 'Dolar Estadounidense' },
+  { codigo: 'MXN', nombre: 'Peso Mexicano' },
+  { codigo: 'GBP', nombre: 'Libras' },
+  { codigo: 'EUR', nombre: 'Euros'}
+]);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>CriptoMonedas</span></h1>
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="" disabled selected>Elige tu moneda</option>
+            <option v-for="moneda in monedas" :key="moneda.codigo" :value="moneda.codigo">{{ moneda.nombre }}</option>
+          </select>
+        </div>
+        <div class="campo">
+          <label for="criptomoneda">CriptoMoneda:</label>
+          <select id="criptomoneda">
+            <option value="" disabled selected>Elige tu criptoMoneda</option>
+            
+          </select>
+        </div>
+      </form>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
