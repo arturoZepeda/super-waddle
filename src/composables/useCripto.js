f8d161a93d5b96f470e31d5d250e809cf1cdabaa
Arturo Zepeda <aaze92@gmail.com>
2023-08-26 22:20:44 -0600

    corrección

--- src/composables/useCripto.js
+++ src/composables/useCripto.js
@@ -1,8 +1,7 @@
 import { ref, onMounted } from 'vue';
 
 export default function useCripto() {
-    
-    
+    const cotizacion = ref({});
     const cargando = ref(false);
     const criptomonedas = ref([]);
     const monedas = ref([
