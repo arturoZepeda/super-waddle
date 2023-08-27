fd134170d9e51a792c4e29e741d765a6cdf8f04e
Arturo Zepeda <aaze92@gmail.com>
2023-08-26 20:57:55 -0600

    añado logica para mostrar datos de cotización

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, reactive } from 'vue';
+import Alerta from './components/Alerta.vue';
 
 const criptomonedas = ref([]);
+const error = ref(false);
+const cotizacion = ref({});
+const cotizar = reactive({
+  moneda: '',
+  criptomoneda: ''
+});
+
 const monedas = ref([
   { codigo: 'USD', nombre: 'Dolar Estadounidense' },
   { codigo: 'MXN', nombre: 'Peso Mexicano' },
   { codigo: 'GBP', nombre: 'Libras' },
   { codigo: 'EUR', nombre: 'Euros'}
 ]);
-
 
 onMounted(async () => {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
@@ -17,6 +24,23 @@ onMounted(async () => {
   criptomonedas.value = resultado.Data;
 });
 
+const cotizarMoneda = () => {
+  if (Object.values(cotizar).includes('')) {
+    error.value = true;
+    return;
+  }
+  error.value = false;
+  obtenCotizacion();
+}
+
+const obtenCotizacion = async()=>{
+  
+    const { moneda, criptomoneda } = cotizar;
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    cotizacion.value = resultado.DISPLAY[criptomoneda][moneda];
+}
 
 
 </script>
@@ -25,10 +49,15 @@ onMounted(async () => {
   <div class="contenedor">
     <h1 class="titulo">Cotizador de <span>CriptoMonedas</span></h1>
     <div class="contenido">
-      <form class="formulario">
+      <Alerta v-if="error">
+        <p>"Los campos de moneda y criptomoneda son obligatorios"</p>
+      </Alerta>
+      <form class="formulario"
+        @submit.prevent="cotizarMoneda"
+      >
         <div class="campo">
           <label for="moneda">Moneda:</label>
-          <select id="moneda">
+          <select id="moneda" v-model="cotizar.moneda">
             <option value="" disabled selected>Elige tu moneda</option>
             <option 
               v-for="moneda in monedas" 
@@ -40,8 +69,8 @@ onMounted(async () => {
         </div>
         <div class="campo">
           <label for="criptomoneda">CriptoMoneda:</label>
-          <select id="criptomoneda">
-            <option value="" disabled selected>Elige tu criptoMoneda</option>
+          <select id="criptomoneda" v-model="cotizar.criptomoneda">
+            <option value="" disabled selected>Elige tu criptomoneda</option>
             <option 
               v-for="criptomoneda in criptomonedas"
               :key="criptomoneda.CoinInfo.Id"
@@ -50,7 +79,24 @@ onMounted(async () => {
             </option>
           </select>
         </div>
+        <div class="boton">
+          <input type="submit" class="btn btn-primario" value="Cotizar">
+        </div>
       </form>
+      <div v-if="cotizacion" class="contendor-resultado">
+        <h2>cotizacion</h2>
+        <div class="resultado">
+          <img :src="'https://cryptocompare.com/'+ cotizacion.IMAGEURL" alt="imagen cripto">
+        <div>
+          <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
+          <p>El precio mas alto del dia: <span>{{ cotizacion.HIGHDAY }}</span></p>
+          <p>El precio mas bajo del dia: <span>{{ cotizacion.LOWDAY }}</span></p>
+          <p>Variacion ultimas 24 horas: <span>{{ cotizacion.CHANGEPCT24HOUR }}</span></p>
+          <p>Ultima actualizacion: <span>{{ cotizacion.LASTUPDATE }}</span></p>
+          
+        </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
